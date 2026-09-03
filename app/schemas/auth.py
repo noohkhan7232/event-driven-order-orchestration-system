48f7fb21b4e9db550955a40c862cdff6ba182805
noohkhan7232 <nooh.khan840@gmail.com>
2026-09-03 12:03:14 +0530

    initial commit

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -20,12 +20,15 @@ class UserRead(BaseModel):
 
     model_config = {"from_attributes": True}
 
+# """
+# class Token(BaseModel):
+#     access_token: str
+#     token_type: str = "bearer"
+#     """
+    
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
 
 
 class LoginRequest(BaseModel):
-    email: EmailStr
+    email: str
     password: str
